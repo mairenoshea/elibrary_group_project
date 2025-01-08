8f00a1e948d26e1d9261b908067fd3889d97e92a
Mairen OShea <mairenoshea@gmail.com>
2025-01-08 00:27:21 -0500

    test commit

---
+++ wireframes/header.jsx
@@ -0,0 +1,14 @@
+// header component 
+
+<div className="header">
+    <div className="headerLinks">
+        <Link to={"/patients"}>
+            <button className="headerLinkButton">Home</button>
+        </Link>
+    </div>
+    <div className="headerTitle">{props.pageTitle}</div>
+    <div className="headerLinks">
+    {props.customButtonName ? 
+    <Link to={`${props.customButtonLink}`}>
+        <button className="headerLinkButton">{props.customButtonName}</button></Link>:null}</div>
+</div>
