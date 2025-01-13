f63e858a7ce359fae7cfe85cb1cbe5987ae82b52
AHcoder <147666340+coochiecoder@users.noreply.github.com>
2025-01-12 22:56:58 -0800

    Create services.js

---
+++ eLibrary/client/src/services/services.js
@@ -0,0 +1,50 @@
+import axios from "axios"
+
+const USER_INSTANCE = axios.create({
+    baseURL: 'http://localhost:8001/api'
+})
+
+
+export const addUser = async (data) => {
+    try {
+        const RES = await USER_INSTANCE.post('/users', data)
+        return RES.data
+    }
+    catch (error) { throw error.response.data.errors }
+}
+
+
+export const getAllUser = async () => {
+    try {
+        const RES = await USER_INSTANCE.get('/users')
+        return RES.data
+    }
+    catch (error) { throw error }
+}
+
+
+
+export const getOneUserByid = async (id) => {
+    try {
+        const RES = await USER_INSTANCE.get(`/users/${id}`)
+        return RES.data
+    } catch (error) { throw error }
+}
+
+
+export const updateOneUser = async (data) => {
+    try {
+        const RES = await USER_INSTANCE.put(`/users/${data._id}`, data)
+        return RES.data
+    }
+    catch (error) { throw error.response.data.errors }
+}
+
+
+export const deleteUserByid = async (id) => {
+    try {
+        const RES = await USER_INSTANCE.delete(`/users/${id}`)
+        return RES.data
+    }
+    catch (error) { throw error }
+}
