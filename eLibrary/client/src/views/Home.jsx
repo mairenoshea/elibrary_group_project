7706a4871d2704b42085ba0a8bf601f3c9a49105
Mairen <169092095+mairenoshea@users.noreply.github.com>
2025-01-14 23:07:04 -0500

    :)

--- eLibrary/client/src/views/Home.jsx
+++ eLibrary/client/src/views/Home.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
+import Header from '../components/Header.jsx'
+import SearchBar from "../components/SearchBar.jsx";
 export const Home = (props) => {
     
 
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Header />
+            
+        </>
     )
 }
 
